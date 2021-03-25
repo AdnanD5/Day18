f30d87c03d81c24176ecb8d23519b3d87dd31cec
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-25 19:25:24 +0100

    Dodavanje ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,19 @@
+let myArr = [];
+//Only change code below this line
+function* multiplication(x){
+    for(i=0; i<3; i++){
+        yield x;
+        x *= 2;
+        yield x;
+        
+        myArr.push(x)
+    }
+    return x
+}
+    var num = multiplication(3);
+    for(number of num){
+        console.log(num.next().value);
+    }
+
+//Only change code above this line
+module.exports = multiplication;
