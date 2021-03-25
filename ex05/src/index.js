761b12dd3c4a47eabc45d04a2486bb3f72eb9b62
AdnanD5 <adodervisevic55@hotmail.com>
2021-03-25 22:05:46 +0100

    d

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+//Only change code below this line
+let shoppingList = new Map([
+    [1, 'Banana'],
+    [2, 'Pineapple'],
+    [3, 'Pear'],
+    [4, 'Carrot'],
+    [5, 'Apple'],
+])
+//Only change code above this line
+module.exports = { shoppingList, arrKey, arrValue};
